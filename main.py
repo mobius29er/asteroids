d789da705c0af7117a817d7b577eea8afafb83e0
Jeremy Foxx <37269405+mobius29er@users.noreply.github.com>
2024-12-06 04:55:04 -0500

    Added sprites class to draw circles

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -32,6 +34,7 @@ def main():
         screen.fill((0, 0, 0))  # Fills the screen with black
         print("foo")
         pygame.display.update()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()

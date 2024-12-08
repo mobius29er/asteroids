186ccdfa21ad481910d37df996e2e3b91aef924c
Jeremy Foxx <37269405+mobius29er@users.noreply.github.com>
2024-12-07 21:10:42 -0500

    E: Corrected player movement

--- player.py
+++ player.py
@@ -25,16 +25,16 @@ class Player(CircleShape):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            self.rotate(dt)
+            self.rotate(-dt)
 
         if keys[pygame.K_d]:
-            self.rotate(-dt)
+            self.rotate(dt)
         
         if keys[pygame.K_w]:
-            self.move(-dt)
+            self.move(dt)
 
         if keys[pygame.K_s]:
-            self.move(dt)
+            self.move(-dt)
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED*dt
